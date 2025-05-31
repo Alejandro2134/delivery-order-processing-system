6dae311e83a7ba1ed9579f9b50e726d07a4159e6
Alejandro Zapata Molina <zapatamolinaalejandro@gmail.com>
2025-05-30 23:20:58 -0500

    feat: seed function added

--- src/infrastructure/db/orm/drizzle/index.ts
+++ src/infrastructure/db/orm/drizzle/index.ts
@@ -1,6 +1,9 @@
 import "../../../../../envConfig";
 import { drizzle, NodePgDatabase } from "drizzle-orm/node-postgres";
+import { seed } from "drizzle-seed";
 import { Pool } from "pg";
+import { clientsTable } from "./schema/client";
+import { restaurantTable } from "./schema/restaurant";
 
 let dbInstance: NodePgDatabase | null = null;
 
@@ -11,7 +14,35 @@ export const db = () => {
 
     const pool = new Pool({ connectionString: url });
     dbInstance = drizzle(pool);
+    populateDb();
   }
 
   return dbInstance;
+};
+
+const populateDb = async () => {
+  const existingClients = await dbInstance!
+    .select()
+    .from(clientsTable)
+    .limit(1);
+
+  if (existingClients.length > 0) return;
+
+  await seed(dbInstance!, { clientsTable, restaurantTable }).refine((f) => ({
+    clientsTable: {
+      columns: {
+        phoneNumber: f.phoneNumber(),
+        address: f.streetAddress(),
+      },
+    },
+    restaurantTable: {
+      columns: {
+        name: f.companyName(),
+        phoneNumber: f.phoneNumber(),
+        address: f.streetAddress(),
+      },
+    },
+  }));
+
+  console.log("Database seeded with initial data.");
 };
