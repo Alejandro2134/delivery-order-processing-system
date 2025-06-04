e257c2d0cbecbe316b04ddd6be2e9bbc0843c416
Alejandro Zapata Molina <zapatamolinaalejandro@gmail.com>
2025-06-04 15:32:02 -0500

    feat: pages update

--- src/app/robots/page.tsx
+++ src/app/robots/page.tsx
@@ -4,6 +4,9 @@ import Link from "next/link";
 const RobotsPage = () => {
   return (
     <main className="p-6">
+      <Link href="/" className="text-blue-600 hover:underline text-sm">
+        ← Go to main dashboard
+      </Link>
       <div className="flex justify-between items-center mb-4">
         <h1 className="text-2xl font-bold">Robot Dashboard</h1>
         <Link href="/orders" className="text-blue-600 hover:underline text-sm">
